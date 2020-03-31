f37ef5f5e7b70c91b7ceda12a857b9630581743a
priyamvada <pranshulpandey5@gmail.com>
2020-03-31 15:43:02 +0100

    adding to server

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const config=require('./config');
 var moment = require('moment');
 const routeData= require('./route/route');
 //const app= express();
+const path=require('path');
 
 mongoose.connect(config.connection_string , {useNewUrlParser: true, useUnifiedTopology: true}).then((client)=>{
 
@@ -17,6 +18,13 @@ mongoose.connect(config.connection_string , {useNewUrlParser: true, useUnifiedTo
 app= express();
 app.use(express.json({ limit: '10MB' }));
 app.use('/api/v1/application', routeData);
+
+const appPath=path.join(__dirname, '..', 'build');
+app.use(express.static(appPath));
+
+app.get('*', function(req, res){
+		res.sendFile(path.resolve(appPath, 'index.html'));
+});
 
 app.listen(process.env.PORT || 3001, function(){
 		console.log('Server is avaiable and listening to port');
